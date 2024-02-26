f2c5b1597879a9231ddee0ffa3a14a0dd5c23da0
Jose Trujillo <joseh.trujillo11@gmail.com>
2024-02-26 11:50:20 -0500

    DONE

---
+++ Emoji Dictonary/Emoji.swift
@@ -0,0 +1,15 @@
+//
+//  Emoji.swift
+//  Emoji Dictonary
+//
+//  Created by 4d on 2/26/24.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
